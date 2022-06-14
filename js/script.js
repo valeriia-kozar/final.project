e7099af0e30218275395eed20fc9597decd48ae1
unknown <Asus-Notebook@DESKTOP-F7LL330.bsrv.com.ua>
2022-06-14 23:09:43 +0300

    third commit

--- js/script.js
+++ js/script.js
@@ -84,39 +84,3 @@ $(document).ready(function(){
  }
 
 
-
-
-
-
-// const animItems = document.querySelectorAll('._anim-items');
-
-// if (animItems.length > 0) {
-//     window.addEventListener('scroll', animOnScroll);
-//     function animOnScroll() {
-//         for (let index = 0; index < animItems.length; index++) {
-//             const animItem = animItems[index];
-//             const animItemHeight = animItem.offsetHeight;
-//             const animItemOffset = offset(animItem).top;
-//             const animStart = 4;
-            
-//             let animItemPoint = window.innerHeight - animItemHeight / animStart;
-//             if (animItemHeight > window.innerHeight) {
-//                 animItemPoint = window.innerHeight - window.innerHeight / animStart;
-//             } 
-
-//             if ((scrollYOffset > animItemOffset - animItemPoint) && scrollYOffset < (animItemOffset + animItemHeight)) {
-//                 animItem.classList.add('_active');
-//             } else {
-//                 animItem.classList.remove('_active')
-//             }
-                
-//         }
-//     }
-//     function offset(el) {
-//         const rect = el.getBoundingClientRect(),
-//             scrollLeft = window.scrollXOffset || document.documentElement.scrollLeft,
-//             scrollTop = window.scrollYOffset || document.documentElement.scrollTop;
-//         return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
-//     }
-// }
-
